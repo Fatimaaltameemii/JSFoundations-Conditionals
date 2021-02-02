fb6c988ed39521a4041913648e962441939a7e2f
Fatimaaltameemii <78157712+Fatimaaltameemii@users.noreply.github.com>
2021-02-02 19:26:27 +0300

    Task 4
    Love you CODED ;)

--- calculator.js
+++ calculator.js
@@ -1,3 +1,10 @@
 // Define the numbers and the operation here
-
+let num1 = 10;
+let num2 = 5;
+let theopration = "-";
 // Write if-statements here
+if (theopration === "-") {
+  console.log(num1 - num2);
+} else {
+  console.log("it's not the same");
+}
